46dfcee70aa35bd512c042fbe1e0fa02bb5a514d
yang1young <1031545274@qq.com>
2017-10-18 14:51:07 +0800

    add binary code compile utils

---
+++ clean_utils/mou_data_prepare.py
@@ -0,0 +1,56 @@
+import os
+import subprocess
+
+origin_path = "/home/qiaoyang/codeData/molili/ProgramData/"
+new_path = "/home/qiaoyang/bisheData/binary_source/"
+binary_path = "/home/qiaoyang/bisheData/binary_out/"
+head = "#include <iostream>\n#include <stdio.h>\n#include <memory.h>\n" \
+       "#include <string.h>\n#include <math.h>\n#include <malloc.h>\n" \
+       "#include <stdlib.h>\n#include <iomanip>\n#include <algorithm>\n" \
+       "using namespace std;\n#define PI 3.14159265\n" \
+       "\n\n"
+
+def reformate(origin_path,new_path):
+    dirs = os.listdir(origin_path)
+    dirs.sort(key = lambda x:int(x))
+    for dir in dirs:
+        files = os.listdir(origin_path+dir)
+        files.sort(key=lambda x: int(str(x).split('.')[0]))
+        if (not os.path.isdir(new_path + dir)):
+            os.mkdir(new_path + dir)
+        if (not os.path.isdir(binary_path + dir)):
+            os.mkdir(binary_path + dir)
+        count = 0
+        for file in files:
+            with open(origin_path+dir+'/'+file,'r') as f_read:
+                code = f_read.read()
+                code = code.replace("void main()","int main()")
+                code = code.replace("void main ()","int main()")
+            with open(new_path+dir+'/'+str(count)+'.cpp','w') as f_write:
+                f_write.write(head+code)
+            count+=1
+
+def compile(path):
+    total = list()
+    dirs = os.listdir(path)
+    dirs.sort(key=lambda x: int(x))
+    for dir in dirs:
+        files = os.listdir(path + dir)
+        files.sort(key=lambda x: int(str(x).split('.')[0]))
+        for file in files:
+            full_source = path+dir+'/'+file
+            full_binary = binary_path+dir+'/'+file.split('.')[0]+'.out'
+            retcode = subprocess.call(["g++",full_source, "-g","-o",full_binary])
+            print dir+'----'+file+'----'+str(retcode)
+            if(retcode==1 and os.path.exists(full_binary)):
+                os.remove(full_binary)
+                print file+"  removed"
+        counts = len(os.listdir(binary_path+dir))
+        total.append(counts)
+    i = 0
+    for l in total:
+        print str(i)+'--'+str(l)
+
+
+reformate(origin_path,new_path)
+compile(new_path)
